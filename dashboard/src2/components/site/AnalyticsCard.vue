324bf90753317a6a0007eb5845cf4fdc4c586de2
ERPtech-prog <biuro@erptech.pl>
2025-07-22 14:19:22 +0200

    feat(dashboard): show average uptime

--- dashboard/src2/components/site/AnalyticsCard.vue
+++ dashboard/src2/components/site/AnalyticsCard.vue
@@ -1,7 +1,12 @@
 <template>
 	<div class="rounded-md border">
 		<div class="flex h-12 items-center justify-between border-b px-5">
-			<h3 class="text-lg font-medium text-gray-900">{{ title }}</h3>
+			<h3 class="text-lg font-medium text-gray-900">
+				{{ title }}
+				<span v-if="subtitle" class="ml-2 text-sm font-normal text-gray-600">
+					{{ subtitle }}
+				</span>
+			</h3>
 			<slot name="action"></slot>
 		</div>
 		<slot></slot>
@@ -11,6 +16,6 @@
 <script>
 export default {
 	name: 'AnalyticsCard',
-	props: ['title']
+	props: ['title', 'subtitle'],
 };
 </script>
