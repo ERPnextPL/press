b670aa6d6cb4895fc0da6b76a67ed5723cce5056
Meszpo <igomisz@gmail.com>
2025-07-22 08:54:52 +0200

    Update SiteAnalyticsUptime.vue

--- dashboard/src/views/site/SiteAnalyticsUptime.vue
+++ dashboard/src/views/site/SiteAnalyticsUptime.vue
@@ -16,10 +16,14 @@
 						d[type.key] === undefined
 							? 'bg-white'
 							: d[type.key] === 1
+							? 'bg-emerald-500'
+							: d[type.key] >= 0.95
 							? 'bg-green-500'
-							: d[type.key] === 0
+							: d[type.key] === 0 || d[type.key] < 0.3
 							? 'bg-red-500'
-							: 'bg-yellow-500'
+							: d[type.key] >= 0.3 && d[type.key] <= 0.6
+							? 'bg-orange-500'
+							: 'bg-yellow-500',
 					]"
 					:title="
 						d[type.key]
